e0cdcd91c6f38859aaf4e2bd99c2266d70859df3
Jeren Raquel <jeraquel101@gmail.com>
2023-05-28 14:57:34 -0700

    fixed license files; linked with makefile

---
+++ src/engine/headers/textHandler.hpp
@@ -0,0 +1,15 @@
+#ifndef TEXTHANDLER_HPP_
+#define TEXTHANDLER_HPP_
+
+// Libs
+#include "../../libs/freetype/ft2build.h"
+
+class TextHandler {
+ private:
+  /*Member vars*/;
+
+ public:
+  TextHandler();
+  ~TextHandler();
+};
+#endif  // TEXTHANDLER_HPP_
