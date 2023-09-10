c50e973f5f82b13f1af8d25519b6b0d0ddb3d40b
Jeren Raquel <jeraquel101@gmail.com>
2023-09-09 23:06:18 -0700

    added init code for texture atlases [untested]

--- templatePackage/build/assets/shaders/fragment.fs
+++ templatePackage/build/assets/shaders/fragment.fs
@@ -1,9 +1,24 @@
 #version 450
 
 uniform vec3 color;
+uniform int useTexture;
+// x: width cell length, y: height cell length, z: x offset, w: y offset 
+uniform vec4 uvInfo;
+uniform sampler2D texture1;
+
+in vec2 TexCoords;
 
 out vec4 fragColor;
 
+vec2 getUV() {
+    vec2 uv = TexCoords;
+    uv.x = uv.x * uvInfo.x + uvInfo.z;
+    uv.y = uv.y * uvInfo.y + uvInfo.w;
+    return uv;
+}
+
 void main() {
-    fragColor = vec4(color, 1.0);
+    vec4 normColor = vec4(color, 1.0) * 1-useTexture
+    vec4 texColor = texture(texture1, getUV) * useTexture;
+    fragColor = normColor + texColor;
 };
--- templatePackage/build/assets/shaders/vertex.vs
+++ templatePackage/build/assets/shaders/vertex.vs
@@ -3,7 +3,11 @@
 uniform mat4 mvp;
 
 in vec3 position;
+in vec2 uvs;
+
+out vec2 TexCoords;
 
 void main() {
     gl_Position = mvp * vec4(position, 1.0);
+    TexCoords = uvs;
 };
