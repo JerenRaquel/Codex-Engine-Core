5e414bbf0432d51c6b8ba0f05b00baac3fcaf50e
Jeren Raquel <jeraquel101@gmail.com>
2023-09-15 18:08:38 -0700

    Update fragment.fs
    - forgot to discard transparent pixels

--- templatePackage/build/assets/shaders/fragment.fs
+++ templatePackage/build/assets/shaders/fragment.fs
@@ -14,14 +14,19 @@ vec2 getUV() {
     vec2 uv = TexCoords;
     int xIndex = uvInfo.z % uvInfo.x;
     int yIndex = uvInfo.z / uvInfo.x;
+    float w = 1.0 / float(uvInfo.x);
+    float h = 1.0 / float(uvInfo.y);
 
-    uv.x = uv.x * uvInfo.x + float(xIndex);
-    uv.y = uv.y * uvInfo.y + float(yIndex);
+    uv.x = uv.x * w + float(xIndex) * w;
+    uv.y = uv.y * h + float(yIndex) * h;
+
     return uv;
 }
 
 void main() {
     vec4 normColor = vec4(color, 1.0) * 1-useTexture;
-    vec4 texColor = texture(texture1, getUV()) * useTexture;
+    vec4 sampledTexture = texture(texture1, getUV());
+    if (sampledTexture.a < 0.1) discard; // discard transparent pixels
+    vec4 texColor = sampledTexture * useTexture;
     fragColor = normColor + texColor;
 };
